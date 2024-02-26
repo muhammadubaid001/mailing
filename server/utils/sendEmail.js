a06f61412d66e1adf279663e67f028a943a9c354
Muhammad Ubaid <39967022+muhammadubaid001@users.noreply.github.com>
2024-02-26 09:57:11 +0500

    Fixing issues

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,5 +1,7 @@
 const nodeMailer = require("nodemailer");
 const path = require("path");
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
 
 const sendEmail = async ({ email, subject, message, data, id }) => {
   const output = `
@@ -21,17 +23,36 @@ const sendEmail = async ({ email, subject, message, data, id }) => {
         </div>
     `;
 
-  // const transporter = nodeMailer.createTransport({
-  //     host: 'smtp.gmail.com',
-  //     port: 465,
-  //     service: process.env.SMTP_SERVICE,
-  //     auth: {
-  //         user: process.env.SMTP_MAIL,
-  //         pass: process.env.SMTP_PASSWORD
-  //     }
-  // })
+  // Create a new PDF document
+  const doc = new PDFDocument();
 
-  console.log(subject)
+  // Add content to the PDF document
+  doc.fontSize(25).text('Generator Information', 60, 80);
+  doc.text(`Client: ${data.client}`, 80, 130);
+  doc.text(`Email: ${data.email}`, 80, 150);
+  doc.text(`Address: ${data.address}`, 80, 170);
+  doc.text(`Phone: ${data.phoneNumber}`, 80, 190);
+  doc.text(`Date: ${data.generalDate}`, 80, 210);
+
+   doc.text("Transporter Information", 60,250);
+  doc.text(`Company: ${data.companyName}`, 80, 270);
+  doc.text(`Items Collected: ${data.representative}`, 80, 290);
+  doc.text(`Name: ${data.transporterName}`, 80, 310);
+  doc.text(`Address: ${data.transporterAddress}`, 80, 330);
+  doc.text(`Phone: ${data.transporterPhoneNumber}`, 80, 410);
+  doc.text(`Date: ${data.transporterDate}`, 80, 430);
+
+  // doc
+  //   .image(path.join(__dirname, "..", `/public/${id}.png`), 0, 15, {
+  //     width: 300,
+  //   })
+  // Save the PDF file to disk
+  doc.pipe(
+    fs.createWriteStream(path.join(__dirname, "..", `/public/${id}.pdf`))
+  );
+
+  // End the document
+  doc.end();
 
   // For Mailtrap
   const transporter = nodeMailer.createTransport({
@@ -55,10 +76,21 @@ const sendEmail = async ({ email, subject, message, data, id }) => {
     html: output,
     attachments: [
       {
-        filename: "sign.png",
-        path: path.join(__dirname, "..", `/public/${id}.png`),
+        filename: "output.pdf",
+        path: path.join(__dirname, "..", `/public/${id}.pdf`),
+        contentType: "application/pdf",
       },
+      {
+            filename: "sign.png",
+            path: path.join(__dirname, "..", `/public/${id}.png`),
+          },
     ],
+    // attachments: [
+    //   {
+    //     filename: "sign.png",
+    //     path: path.join(__dirname, "..", `/public/${id}.png`),
+    //   },
+    // ],
   };
 
   await transporter.sendMail(mailOptions);
