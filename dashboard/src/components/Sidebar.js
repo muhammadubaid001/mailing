5ce433b1fe2a8c58fda38115ecc3a90fd6723f56
Muhammad Ubaid <39967022+muhammadubaid001@users.noreply.github.com>
2024-03-06 12:31:14 +0500

    Added range filters

--- dashboard/src/components/Sidebar.js
+++ dashboard/src/components/Sidebar.js
@@ -1,22 +1,78 @@
 import { Input, Typography } from "@material-tailwind/react";
+import { useState } from "react";
 
 export const Sidebar = ({ data, state, setState, setReferenceNumber }) => {
+  const [filters, setFilters] = useState({
+    from: null,
+    to: null,
+  });
   const handleClick = (item) => {
-    setState(item)
+    setState(item);
   };
+
+  const filterDataByDateRange = () => {
+    if (filters.from && filters.to) {
+      return data.filter((item) => {
+        const createdAtDate = new Date(item.createdAt);
+        return (
+          createdAtDate >= new Date(filters.from) &&
+          createdAtDate <= new Date(filters.to)
+        );
+      });
+    } else {
+      return data;
+    }
+  };
+
+
   return (
-    <div className="w-72 overflow-auto hidden md:block bg-white shadow-sm p-5">
-      <Typography variant="h6">References</Typography>
+    <div className="w-80 overflow-auto hidden md:block bg-white shadow-sm p-5">
+      <Typography variant="h6" className="mb-1">
+        Date Range
+      </Typography>
+      <div className="flex gap-2">
+        <Input
+          type="date"
+          label="From"
+          onChange={(e) =>
+            setFilters({
+              ...filters,
+              from: e.target.value,
+            })
+          }
+          containerProps={{
+            className: "!min-w-0",
+          }}
+        />
+        <Input
+          type="date"
+          onChange={(e) =>
+            setFilters({
+              ...filters,
+              to: e.target.value,
+            })
+          }
+          label="To"
+          containerProps={{
+            className: "!min-w-0",
+          }}
+        />
+      </div>
+      <Typography variant="h6" className="mt-2">
+        References
+      </Typography>
       <Input
         onChange={(e) => setReferenceNumber(e.target.value)}
         label="Search"
       />
       <div className="flex flex-col gap-2 mt-4">
-        {data?.map((item) => (
+        {filterDataByDateRange()?.map((item) => (
           <div
             key={item._id}
             onClick={() => handleClick(item)}
-            className={`cursor-pointer bg-gray-100 text-zinc-900 rounded-lg p-2 hover:bg-blue-100 ${item._id === state._id ? 'bg-blue-200' : ''}`}
+            className={`cursor-pointer bg-gray-100 text-zinc-900 rounded-lg p-2 hover:bg-blue-100 ${
+              item._id === state._id ? "bg-blue-200" : ""
+            }`}
           >
             <Typography className="text-sm">
               Ref #: {item?.referenceNumber}
