b36d52b6e2b73c8a02d7279faa54cab3a7765e5b
Muhammad Ubaid <39967022+muhammadubaid001@users.noreply.github.com>
2024-03-06 12:01:55 +0500

    Fixing pdf

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -26,22 +26,38 @@ const sendEmail = async ({ email, subject, message, data, id, buffer }) => {
   // Create a new PDF document
   const doc = new PDFDocument();
 
+  const image = data?.companyName === "Synergy Environmental" ? path.join(__dirname, "..", "/public/PHOTO-2024-03-06-01-39-22.jpg") : path.join(__dirname, "..",  "/public/PHOTO-2024-03-06-01-39-23 2.jpg")
+  doc.image(
+    image,
+    {
+      fit: [200, 100],
+      align: "center",
+      valign: "center",
+    },
+  );
+
   // Add content to the PDF document
-  doc.font("Times-Bold").text("Generator Information", 60, 80);
-  doc.text(`Client: ${data.client}`, 80, 110);
-  doc.text(`Email: ${data.email}`, 80, 130);
-  doc.text(`Address: ${data.address}`, 80, 150);
-  doc.text(`Phone: ${data.phoneNumber}`, 80, 170);
-  doc.text(`Date: ${data.generalDate}`, 80, 190);
+  doc.font("Times-Bold").fontSize(20).text("Regulated Medical Waste Manifest", 60, 200);
+  doc.moveDown();
+  doc.fontSize(14)
+  doc.font("Times-Bold").text("Generator Information", 60, 230);
+  doc.font("Times-Roman")
+  doc.text(`Client: ${data.client}`, 80, 250);
+  doc.text(`Email: ${data.email}`, 80, 270);
+  doc.text(`Address: ${data.address}`, 80, 290);
+  doc.text(`Phone: ${data.phoneNumber}`, 80, 310);
+  doc.text(`Date: ${data.generalDate}`, 80, 330);
 
   doc.moveDown();
-  doc.font("Times-Bold").text("Transporter Information", 60, 220);
-  doc.text(`Company: ${data.companyName}`, 80, 240);
-  doc.text(`Items Collected: ${data.representative}`, 80, 260);
-  doc.text(`Name: ${data.transporterName}`, 80, 280);
-  doc.text(`Address: ${data.transporterAddress}`, 80, 300);
-  doc.text(`Phone: ${data.transporterPhoneNumber}`, 80, 320);
-  doc.text(`Date: ${data.transporterDate}`, 80, 340);
+  doc.font("Times-Bold").text("Transporter Information", 60, 350);
+  doc.font("Times-Roman")
+  doc.text(`Company: ${data.companyName}`, 80, 370);
+  doc.text(`Items Collected: ${data.representative}`, 80, 390);
+  doc.text(`Name: ${data.transporterName}`, 80, 410);
+  doc.text(`Address: ${data.transporterAddress}`, 80, 430);
+  doc.text(`Phone: ${data.transporterPhoneNumber}`, 80, 450);
+  doc.text(`Date: ${data.transporterDate}`, 80, 470);
+  doc.text("I certify that the contents of this shipment are fully and accurately described, labeled and are in proper condition for transportation according to the applicable state and federal regulations.", 60, 490);
 
   // doc.pipe(
   //   fs.createWriteStream(path.join(__dirname, "..", `/public/${id}.PNG`))
@@ -56,7 +72,7 @@ const sendEmail = async ({ email, subject, message, data, id, buffer }) => {
   doc.image(
     buffer,
     {
-      fit: [100, 100],
+      fit: [200, 100],
       align: "center",
       valign: "center",
     }
